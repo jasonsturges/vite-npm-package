a7cb626b19c69251ad1d4550d59ae1c394178d51
Jason Sturges <jason@jasonsturges.com>
2024-02-19 22:28:22 -0600

    Example export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 // Main library exports - these are packaged in your distributable
-export const sum = (x, y) => {
-  return x + y;
+
+/**
+ * This function checks if a number is odd.
+ *
+ * @param {number} n - The number to check.
+ * @returns {boolean} - Returns true if the number is odd, false otherwise.
+ *
+ * @example
+ * isOdd(3); // returns true
+ *
+ * @example
+ * isOdd(2); // returns false
+ */
+export const isOdd = (n) => {
+  return !!(n & 1);
 };
